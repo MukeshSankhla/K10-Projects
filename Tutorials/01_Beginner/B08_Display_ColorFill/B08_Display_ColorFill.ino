c903b1b04856b173a358056967c8b7ef0d90ea02
MukeshSankhla <mukeshdiy1@gmail.com>
2026-09-23 19:10:49 +0530

    feat: add B08_Display_ColorFill tutorial project for screen color cycling

--- Tutorials/01_Beginner/B08_Display_ColorFill/B08_Display_ColorFill.ino
+++ Tutorials/01_Beginner/B08_Display_ColorFill/B08_Display_ColorFill.ino
@@ -4,20 +4,20 @@ UNIHIKER_K10 k10;
 uint8_t screen_dir = 2;
 
 void setup() {
-    k10.begin();
-    k10.initScreen(screen_dir);
-    k10.creatCanvas();
+  k10.begin();
+  k10.initScreen(screen_dir);
+  k10.creatCanvas();
 }
 
 void loop() {
-    k10.setScreenBackground(0xFFFFFF);
-    delay(2000);
-    k10.setScreenBackground(0xFF0000);
-    delay(2000);
-    k10.setScreenBackground(0x00FF00);
-    delay(2000);
-    k10.setScreenBackground(0x0000FF);
-    delay(2000);
-    k10.setScreenBackground(0x000000);
-    delay(2000);
+  k10.setScreenBackground(0xFFFFFF);
+  delay(2000);
+  k10.setScreenBackground(0xFF0000);
+  delay(2000);
+  k10.setScreenBackground(0x00FF00);
+  delay(2000);
+  k10.setScreenBackground(0x0000FF);
+  delay(2000);
+  k10.setScreenBackground(0x000000);
+  delay(2000);
 }
